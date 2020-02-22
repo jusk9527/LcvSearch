6ce61109fc15624a51d6338681d116a338a83ae1
jusk? <dreampeoplexyh@qq.com>
2020-02-22 21:49:33 +0800

    Initial commit

---
+++ ven/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator\Downloads\spider-master\LcvSearch\ven\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
